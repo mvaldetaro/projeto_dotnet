8d4b4a6d8f7b6588da5cfc15794559eaf85adb5e
Magno Valdetaro <mvaldetaro@gmail.com>
2018-04-22 09:18:24 -0300

    Registro e Auth concluidos

--- pratica/SocialNetwork/SocialNetwork.Web/Controllers/AccountController.cs
+++ pratica/SocialNetwork/SocialNetwork.Web/Controllers/AccountController.cs
@@ -1,5 +1,8 @@
-﻿using SocialNetwork.Web.Models.Account;
+﻿using Newtonsoft.Json.Linq;
+using SocialNetwork.Web.Helpers;
+using SocialNetwork.Web.Models.Account;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -10,18 +13,18 @@ namespace SocialNetwork.Web.Controllers
     public class AccountController : Controller
     {
         private HttpClient _client;
-
+        private TokenHelper _tokenHelper;
 
         public AccountController()
         {
             _client = new HttpClient();
 
-            _client.BaseAddress = new Uri("https://localhost:44333/");
-            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.BaseAddress = new Uri("https://localhost:44363/");
+            _client.DefaultRequestHeaders.Clear();
 
             var mediaType = new MediaTypeWithQualityHeaderValue("application/json");
             _client.DefaultRequestHeaders.Accept.Add(mediaType);
-            
+            _tokenHelper = new TokenHelper();
         }
 
 
@@ -44,12 +47,52 @@ namespace SocialNetwork.Web.Controllers
                 if(response.IsSuccessStatusCode)
                 {
 
-                } else
+                }
+                else
                 {
 
                 }
             }
 
+            return View(model);
+        }
+
+        //GET: Account/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //POST: Account/Login
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var data = new Dictionary<string, string>()
+                {
+                    { "grant_type", "password" },
+                    { "username", model.Email },
+                    { "password", model.Password }
+                };
+
+                using (var requestContent = new FormUrlEncodedContent(data))
+                {
+                    var response = await _client.PostAsync("/Token", requestContent);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var responseContent = await response.Content.ReadAsStringAsync();
+                        var tokenData = JObject.Parse(responseContent);
+                        _tokenHelper.AccessToken = tokenData["access_token"];
+                    } else
+                    {
+                        ModelState.AddModelError("", "");
+                    }
+                }
+            }
             return View(model);
         }
 
