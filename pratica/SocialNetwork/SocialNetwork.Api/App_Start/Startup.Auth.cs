c2cbb11b84d8bf498e5baad2a9b0516845c4efa1
Magno Valdetaro <mvaldetaro@gmail.com>
2018-04-21 22:19:31 -0300

    Define API e Auth

---
+++ pratica/SocialNetwork/SocialNetwork.Api/App_Start/Startup.Auth.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using SocialNetwork.Api.Models;
+using SocialNetwork.Api.Providers;
+using SocialNetwork.Api.App_Start;
+using System;
+
+
+namespace SocialNetwork.Api
+{
+    public partial class Startup
+    {
+        public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+
+
+            OAuthOptions = new OAuthAuthorizationServerOptions()
+            {
+                Provider = new ApplicationOAuthProvider(),
+                TokenEndpointPath = new PathString("/Token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1)
+            };
+
+            app.UseOAuthBearerTokens(OAuthOptions);
+        }
+    }
+}
--- pratica/SocialNetwork/SocialNetwork.Api/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SocialNetwork.Api.WebApiApplication" Language="C#" %>
